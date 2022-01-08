1a4493f6f14453a36c8164ed23a4794b18a2e74d
ehfehfdl0927 <kimjoohe10@naver.com>
2022-01-09 00:16:59 +0900

    회원서비스테스트

---
+++ spring-project/src/main/java/kimjoohe/springproject/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package kimjoohe.springproject.repository;
+
+import kimjoohe.springproject.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ spring-project/src/main/java/kimjoohe/springproject/service/MemberService.java
@@ -0,0 +1,38 @@
+package kimjoohe.springproject.service;
+
+import kimjoohe.springproject.domain.Member;
+import kimjoohe.springproject.repository.MemberRepository;
+import kimjoohe.springproject.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+        //이렇게 하면 MemberService를 선언할때 외부에서 레파지토리를 넣어줄 수 있음
+    }
+
+    public Long join(Member member){ //회원가입
+        //같은 이름이 있는 중복 회원X
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
